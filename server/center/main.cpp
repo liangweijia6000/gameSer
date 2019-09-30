c9c1334d629345950cde2732212c18d42517d92d
liangweijia6000 <452434058@qq.com>
2019-09-30 09:12:10 +0800

    typedef

--- server/center/main.cpp
+++ server/center/main.cpp
@@ -3,17 +3,17 @@
 
 void test()
 {
-    printf("sizeof char8 :%d\n", sizeof(char8));
-    printf("sizeof char16 :%d\n", sizeof(char16));
-    printf("sizeof float32 :%d\n", sizeof(float32));
-    printf("sizeof double64 :%d\n", sizeof(double64));
-    printf("sizeof byte8 :%d\n", sizeof(byte8));
-    printf("sizeof int16 :%d\n", sizeof(int16));
-    printf("sizeof uint16 :%d\n", sizeof(uint16));
-    printf("sizeof int32 :%d\n", sizeof(int32));
-    printf("sizeof uint32 :%d\n", sizeof(uint32));
-    printf("sizeof int64 :%d\n", sizeof(int64));
-    printf("sizeof uint64 :%d\n", sizeof(uint64));
+    printf("sizeof char8 :%lu\n", sizeof(char8));
+    printf("sizeof char16 :%lu\n", sizeof(char16));
+    printf("sizeof float32 :%lu\n", sizeof(float32));
+    printf("sizeof double64 :%lu\n", sizeof(double64));
+    printf("sizeof byte8 :%lu\n", sizeof(byte8));
+    printf("sizeof int16 :%lu\n", sizeof(int16));
+    printf("sizeof uint16 :%lu\n", sizeof(uint16));
+    printf("sizeof int32 :%lu\n", sizeof(int32));
+    printf("sizeof uint32 :%lu\n", sizeof(uint32));
+    printf("sizeof int64 :%lu\n", sizeof(int64));
+    printf("sizeof uint64 :%lu\n", sizeof(uint64));
 }
  
 int main(int argc, char* argv[])
--- server/share/common/servercommon.h
+++ server/share/common/servercommon.h
@@ -44,5 +44,64 @@ typedef unsigned long long uint64;
 #endif //__amd64
 #endif //__linux__
 
+typedef std::string String;
+typedef std::vector<String> StringVector;  
+typedef std::list<String> StringList; 
+typedef std::map<String,String> StringMap; 
+typedef std::map<String,int32> StringIntMap; 
+typedef std::map<String,int64> StringInt64Map; 
+typedef std::map<String,int64> StringIdMap; 
+typedef std::map<String, float32>StringFloatMap;
+
+typedef std::vector<int32> Int32Vector;  
+typedef std::vector<float32> Float32Vector;
+typedef std::vector<int32> IntVector;  
+typedef std::list<int32> Int32List;   
+typedef std::list<int32> IntList;  
+typedef std::list<int64> Int64List;
+typedef std::list<int64> IdList;
+typedef std::vector<int64> Int64Vector;
+typedef std::vector<int64> IdVector;
+typedef std::vector<Int32Vector> Int32VectorVector;
+typedef std::vector<Int32Vector> IntVectorVector;
+
+typedef std::map<int32,int32> Int32Int32Map;
+typedef std::map<int32,int32> IntIntMap;
+typedef std::map<int64,int32> Int64Int32Map;
+typedef std::map<int64,int32> IdIntMap;
+typedef std::map<int32,int64> Int32Int64Map;
+typedef std::map<int32,int64> IntIdMap;
+typedef std::map<int64,int64> Int64Int64Map;
+typedef std::map<int64,int64> IdIdMap;
+
+typedef std::map<int64,String> Int64StringMap;
+typedef std::map<int64,String> IdStringMap;
+typedef std::map<int32,String> IntStringMap;
+
+typedef std::set<int32> Int32Set;
+typedef std::set<int32> IntSet;
+typedef std::set<int64> Int64Set;
+typedef std::set<int64> IdSet;
+typedef std::set<String> StringSet;
+
+typedef std::map<int64,Int32Int32Map> Int64Int32Int32Map;
+typedef std::map<int32,Int32Int32Map> Int32Int32Int32Map;
+typedef std::map<int32,Int64Int32Map> Int32Int64Int32Map;
+
+typedef std::map<int64,Int32Vector> Int64_Int32VectorMap;
+typedef std::map<int32, Int32Vector> Int32VectorMap;
+typedef std::vector<byte8> Byte8Vector;
+typedef std::vector<int16> Int16Vector;
+typedef std::vector<bool> BoolVector;
+
+
+
+#define FOR_I_INT(size) for (int_32 i = 0; i < size; i++)
+#define FOR_I_UINT(size) for (uint_32 i = 0; i < size; i++)
+#define FOR_K_INT(size) for (int_32 k = 0; k < size; k++)
+#define FOR_K_UINT(size) for (uint_32 k = 0; k < size; k++)
+
+
 
 #endif //_SERVER_COMMON_H_
+
