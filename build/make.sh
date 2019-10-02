df91086b505b83e83ce29dfec8fcba6b3b56bcb5
liangweijia6000 <452434058@qq.com>
2019-10-02 23:06:51 +0800

    sth.

--- build/make.sh
+++ build/make.sh
@@ -1,15 +1,12 @@
 
 startTime=$(date +%s)
 stms=$((startTime*1000+`date "+%N"`/1000000))
-echo "startTime $stms"
 
-make
+make -j8
 
 endTime=$(date +%s)
 etms=$((endTime*1000+`date "+%N"`/1000000))
 
 costTime=$(($etms-stms))
-
-echo "endTime $etms"
 
 echo "takes $costTime ms"
